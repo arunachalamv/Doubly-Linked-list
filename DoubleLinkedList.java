bf358e8192140aa63873147ef151ef5c71772907
Arunachalam Balaji <arun.13.11.1996@gmail.com>
2017-08-14 10:58:06 +0530

    Add files via upload

---
+++ DoubleLinkedList.java
@@ -0,0 +1,226 @@
+import java.util.NoSuchElementException;
+
+
+public class DoubleLinkedList<E> {
+
+	 	private Node head;
+
+	    private Node tail;
+
+	    private int size;
+
+	    public DoubleLinkedList() {
+
+	        size = 0;
+
+	    }
+
+	    private class Node {
+
+	        E element;
+
+	        Node next;
+
+	        Node prev;
+
+	 
+	        public Node(E element, Node next, Node prev) {
+
+	            this.element = element;
+
+	            this.next = next;
+
+	            this.prev = prev;
+
+	        }
+
+	    }
+
+	       public int size() { return size;
+ }
+	     
+
+	    public boolean isEmpty() {
+ return size == 0; 
+}
+
+	     
+	    
+   public void addFirst(E element) {
+
+	        Node tmp = new Node(element, head, null);
+
+	        if(head != null )
+ {
+head.prev = tmp;
+}
+
+	        head = tmp;
+
+	        if(tail == null)
+ {
+ tail = tmp;
+}
+
+	        size++;
+	
+        System.out.println("****adding: "+element);
+
+	    }
+
+	     
+	     public void addLast(E element) 
+{
+	
+         
+	        Node tmp = new Node(element, null, tail);
+
+	        if(tail != null){
+tail.next = tmp;
+}
+
+	        tail = tmp;
+
+	        if(head == null) 
+{
+ head = tmp;
+}
+
+	        size++;
+
+	        System.out.println("****adding: "+element);
+
+	    }
+	    
+    public void iterateForward(){
+
+	         
+	        System.out.println("*****iterating forwards..");
+
+	        Node tmp = head;
+
+	        while(tmp != null){
+
+	            System.out.println(tmp.element);
+
+	            tmp = tmp.next;
+
+	        }
+
+	    }
+	 
+    
+	   
+	    public void iterateBackward(){
+
+	         
+	        System.out.println("*****iterating backwards..");
+
+	        Node tmp = tail;
+
+	        while(tmp != null){
+
+	            System.out.println(tmp.element);
+
+	            tmp = tmp.prev;
+	        
+}
+	    
+}
+
+	    public E removeElement(E element){
+
+	        if (size == 0) throw new NoSuchElementException();
+
+	        Node tmp = head;
+
+	        Node tmp2 = head;
+ 
+	        while (tmp.next!=null){
+	  
+      	if (tmp.element==element){
+	     
+   		if(tmp.prev!=null) tmp.prev.next = tmp.next;
+ 
+	        		if(tmp.next !=null) tmp.next.prev = tmp.prev;
+
+	        		tmp2 = tmp.prev;
+	        		
+tmp = null; 
+
+	        		System.out.println("*****deleted "+element);
+
+	        		break; 
+	        	
+}
+
+	        	tmp = tmp.next; 
+
+	        }
+	
+        return tmp2.element; 
+
+	    }
+	  
+  public E removeFirst() {
+
+	        if (size == 0) throw new NoSuchElementException();
+
+	        Node tmp = head;
+
+	        head = head.next;
+
+	        head.prev = null;
+
+	        size--;
+	      
+  System.out.println("*****deleted: "+tmp.element);
+
+	        return tmp.element;
+
+	    }
+	
+     
+	      public E removeLast() {
+
+	        if (size == 0) throw new NoSuchElementException();
+	
+        Node tmp = tail;
+	      
+  tail = tail.prev;
+	
+        tail.next = null;
+
+	        size--;
+
+	        System.out.println("*****deleted: "+tmp.element);
+
+	        return tmp.element;
+
+	    }
+	     
+	    
+public static void main(String a[]){
+
+	         
+	        DoubleLinkedList<String> dll = new DoubleLinkedList<String>();
+
+	        dll.addFirst("David");
+
+	        dll.addLast("Joshua");
+
+	        dll.addLast("Miriam");
+
+	        dll.iterateForward();
+
+	        dll.removeElement("Joshua");
+	
+        //dll.removeFirst();
+	 
+       //dll.removeLast();
+
+	        dll.iterateBackward();
+	
+    }
+
+}
